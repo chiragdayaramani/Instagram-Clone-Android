9c903258aca6a0b6e0f1073196014241c9989a6f
CHIRAG <chiragddayaramani@gmail.com>
2022-01-11 23:40:13 +0530

    HomeFragment updated

---
+++ app/src/main/java/com/example/instagramclone/Adapter/PostAdapter.java
@@ -0,0 +1,117 @@
+package com.example.instagramclone.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.instagramclone.Model.Post;
+import com.example.instagramclone.Model.User;
+import com.example.instagramclone.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.hendraanggrian.appcompat.widget.SocialTextView;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Post> mPost;
+
+    private FirebaseUser firebaseUser;
+
+    public PostAdapter(Context context, List<Post> mPost) {
+        this.context = context;
+        this.mPost = mPost;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.post_item,parent,false);
+        return new PostAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+
+        Post post= mPost.get(position);
+        Picasso.get().load(post.getImageUrl()).into(holder.postImage);
+        holder.description.setText(post.getDescription());
+
+        FirebaseDatabase.getInstance().getReference().child("Users").child(post.getPublisher()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user=snapshot.getValue(User.class);
+
+                if(user.getImageUrl().equals("default")){
+                    holder.imageProfile.setImageResource(R.mipmap.ic_launcher);
+                }else{
+                    Picasso.get().load(user.getImageUrl()).into(holder.imageProfile);
+                }
+
+
+                holder.username.setText(user.getUsername());
+                holder.author.setText(user.getName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPost.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView imageProfile;
+        public ImageView postImage;
+        public ImageView like;
+        public ImageView comment;
+        public ImageView save;
+        public ImageView more;
+
+        public TextView username;
+        public TextView author;
+        public TextView noOfLikes;
+        public TextView noOfComments;
+        SocialTextView description;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageProfile=itemView.findViewById(R.id.image_profile);
+            postImage=itemView.findViewById(R.id.post_image);
+            like=itemView.findViewById(R.id.like);
+            comment=itemView.findViewById(R.id.comment);
+            save=itemView.findViewById(R.id.save);
+            more=itemView.findViewById(R.id.more);
+
+            username=itemView.findViewById(R.id.username);
+            noOfComments=itemView.findViewById(R.id.no_of_comments);
+            noOfLikes=itemView.findViewById(R.id.no_of_likes);
+            author=itemView.findViewById(R.id.author);
+            description=itemView.findViewById(R.id.description);
+
+
+        }
+    }
+}
